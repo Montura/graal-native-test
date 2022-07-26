c4eba05098e57c334496b52cc8919a54a221005c
montura <fatum.est.series.causarum@gmail.com>
2022-07-26 10:28:30 +0300

    Linux: add missed JNI headers, fix Cmake, init Time in main.cpp

--- native/main.cpp
+++ native/main.cpp
@@ -1,30 +1,26 @@
-#include <iostream>
-
-#include <stdio.h>
-#include <time.h>
+#include <cstdio>
+#include <ctime>
 #include "graal/graal_isolate.h"
 
-
-
 extern "C" void test(graal_isolatethread_t*);
-extern "C" int testC() {
-    return 17;
-}
+extern "C" int testC();
 
 int main() {
     graal_isolate_t* isolate;
     graal_isolatethread_t* thread;
-    graal_create_isolate(0, &isolate, &thread);
+    graal_create_isolate(nullptr, &isolate, &thread);
     test(thread);
 
     int n = 10000000;
     for (int i = 0; i < n; i++) {
         testC();
     }
-    struct timespec time1;
-        for (int i = 0; i < n; i++) {
+    timespec time1;
+    clock_gettime(CLOCK_MONOTONIC, &time1);
+    for (int i = 0; i < n; i++) {
         testC();
     }
-    struct timespec time2;
-    printf("native time = %d\n", ((time2.tv_sec - time1.tv_sec) * 1000000000 + (time2.tv_nsec - time1.tv_nsec)) / n);
+    timespec time2;
+    clock_gettime(CLOCK_MONOTONIC, &time2);
+    printf("native time = %ld\n", ((time2.tv_sec - time1.tv_sec) * 1000000000 + (time2.tv_nsec - time1.tv_nsec)) / n);
 }
