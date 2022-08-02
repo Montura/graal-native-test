ebd587eba278e07f2d1b8e21182bf130f514587f
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2022-08-02 10:05:03 +0300

    Clean up example

--- native/main.cpp
+++ native/main.cpp
@@ -1,31 +1,11 @@
-#include <ctime>
 #include "graal/c_api.h"
 #include "system_properties.h"
 
-void testLib();
-extern "C" void test(graal_isolatethread_t*);
-extern "C" int testC();
-
-
+// how to run: ./native -Ddxfeed.address=172.13.14.15
 int main(int argc, char** argv) {
     graal_isolatethread_t* thread = NativeTestMain_createIsolate();
-    if (argc == 2) {
-        // todo: ./native -Ddxfeed.address=172.13.14.15
+    if (thread && argc == 2) {
         testSystemProperties(argc, argv, thread);
+        graal_tear_down_isolate(thread);
     }
-    testLib();
-    test(thread);
-
-    int n = 10000000;
-    for (int i = 0; i < n; i++) {
-        testC();
-    }
-    timespec time1{};
-    timespec_get(&time1, TIME_UTC);
-    for (int i = 0; i < n; i++) {
-        testC();
-    }
-    timespec time2{};
-    timespec_get(&time2, TIME_UTC);
-    printf("native time = %ld\n", ((time2.tv_sec - time1.tv_sec) * 1000000000 + (time2.tv_nsec - time1.tv_nsec)) / n);
 }
