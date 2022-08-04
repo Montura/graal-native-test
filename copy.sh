df7efd74f49d45f863843b97b4c18a362d783d20
Andrei Mikhalev <amikhalev@devexperts.com>
2022-08-04 17:32:40 +0300

    Try to fix source directory usage in copy.sh

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 srcPath=$1
 dstPath=$2
 
-graalSample_o=$(find "$srcPath" -name GraalSample.o)
+graalSample_o=$(find $srcPath -name GraalSample.o)
 
 echo "Copying to $dstPath:"
 [ -f "$graalSample_o" ] && cp $graalSample_o $dstPath
