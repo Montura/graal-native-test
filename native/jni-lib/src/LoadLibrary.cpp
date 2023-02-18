a6c418297cab3a77961835897136ac4b9e6178f8
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-02-18 23:10:28 +0300

    Make dxfeed::internal::loadJVMLibrary cross-platform

--- native/jni-lib/src/LoadLibrary.cpp
+++ native/jni-lib/src/LoadLibrary.cpp
@@ -1,20 +1,119 @@
+#include <sstream>
 #include <iostream>
 #include <filesystem>
 #include <jni.h>
 
+#include "api/utils/LoadLibrary.h"
+
+namespace fs = std::filesystem;
+
 #if _MSC_VER && !__INTEL_COMPILER
 #include <Windows.h>
 #include <codecvt>
+
+const wchar_t JAVA_DLL_NAME[] = L"java.dll";
+const wchar_t JVM_DLL_NAME[] = L"jvm.dll";
+
+std::wstring utf8_decode(const char* str) {
+    return std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(str);
+}
+
+std::wstring javaLibraryPath(const char* javaHome) {
+  auto java_home = utf8_decode(javaHome);
+  return java_home + JAVA_DLL_NAME;
+}
+
+std::string jvmLibraryPath(const char* javaHome) {
+  auto java_home = utf8_decode(javaHome);
+  return java_home + JVM_DLL_NAME;
+}
+
+HINSTANCE openLib(const char* path) {
+  HINSTANCE libraryHandle = LoadLibraryW(path);
+  if (!libraryHandle) {
+    std::cerr << "Can't load lib from: " << path << "\n";
+  }
+  std::cout << "Loaded library: " << path << "\n";
+  return libraryHandle;
+}
+
+template <typename SymbolType, typename Handle>
+SymbolType loadSymbol(HINSTANCE libraryHandle, const char* symbolName) {
+  auto symbol = (SymbolType) GetProcAddress(libraryHandle, symbolName);
+  if (!symbol) {
+    std::string errMsg("Can't load lib from: ");
+    throw std::runtime_error(errMsg + symbolName);
+  }
+  return symbol;
+}
 #else
 
 #include <dlfcn.h>
 #include <string>
 
+template <class TargetType, class InitialType>
+constexpr inline TargetType r_cast(InitialType arg) {
+  return reinterpret_cast<TargetType>(arg);
+}
+
+const char JAVA_DLL_NAME[] = "/lib/libjava.dylib";
+const char JVM_DLL_NAME[] = "/lib/server/libjvm.dylib";
+
+std::string javaLibraryPath(const char* javaHome) {
+  std::string java_home(javaHome);
+  return java_home + JAVA_DLL_NAME;
+}
+
+std::string jvmLibraryPath(const char* javaHome) {
+  std::string java_home(javaHome);
+  return java_home + JVM_DLL_NAME;
+}
+
+void* openLib(const char* path) {
+  void* libraryHandle = dlopen(path, RTLD_LAZY);
+  if (!libraryHandle) {
+    std::string errMsg("Can't load lib from: ");
+    throw std::runtime_error(errMsg + path);
+  }
+  std::cout << "Loaded library: " << path << "\n";
+  return libraryHandle;
+}
+
+template <typename SymbolType>
+SymbolType loadSymbol(void* libraryHandle, const char* symbolName) {
+  auto symbol = (SymbolType) dlsym(libraryHandle, symbolName);
+  if (!symbol) {
+    std::stringstream ss{};
+    ss << "Can't load symbol" << symbolName << " from library:" << libraryHandle;
+    throw std::runtime_error(ss.str());
+  }
+  return symbol;
+}
+
 #endif
 
-#include "api/utils/LoadLibrary.h"
+namespace dxfeed::internal {
+  CreateJavaVM_t createJavaVM = nullptr;
 
-namespace fs = std::filesystem;
+  void loadJVMLibrary(const char* javaHome) {
+    auto javaDllPath = javaLibraryPath(javaHome);
+    bool file_exists = fs::exists(javaDllPath);
+    auto size = file_exists && fs::is_regular_file(javaDllPath) ? static_cast<int64_t>(fs::file_size(javaDllPath)) : 0;
+    if (!size) {
+      throw std::runtime_error("File don't exist: " + javaDllPath);
+    }
+
+    auto jvmDllPath = jvmLibraryPath(javaHome);
+    auto JVM_DLL = openLib(jvmDllPath.c_str());
+    createJavaVM = loadSymbol<CreateJavaVM_t>(JVM_DLL, "JNI_CreateJavaVM");
+    auto findBootClass = loadSymbol<FindClassFromBootLoader_t>(JVM_DLL, "JVM_FindClassFromBootLoader");
+    if (createJavaVM == nullptr || findBootClass == nullptr) {
+      std::stringstream ss{};
+      ss << "GetProcAddress failed: " << r_cast<void*>(createJavaVM) << ", " << r_cast<void*>(findBootClass);
+      throw std::runtime_error(ss.str());
+    }
+  }
+}
 
 // todo: at least need
 //      server/jvm.dll
@@ -23,65 +122,3 @@ namespace fs = std::filesystem;
 //      jsvml.dll
 //      verify.dll
 //      lib/modules
-
-namespace dxfeed::internal {
-  CreateJavaVM_t createJavaVM = nullptr;
-
-#if _MSC_VER && !__INTEL_COMPILER
-  // todo: replace with platform code
-      std::wstring utf8_decode(const char* str) {
-          return std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(str);
-      }
-#endif
-
-  // todo: extract platform specific
-  void loadJVMLibrary(const char* javaHome) {
-#if _MSC_VER && !__INTEL_COMPILER
-    std::wstring java_home = utf8_decode(javaHome);
-    auto javaDllPath = java_home + L"\\bin\\java.dll";
-    bool file_exists = fs::exists(javaDllPath);
-    auto size = file_exists && fs::is_regular_file(javaDllPath) ? static_cast<int64_t>(fs::file_size(javaDllPath)) : 0;
-    if (!size) {
-        std::cerr << "File don't exist: " << javaDllPath.c_str() << "\n";
-    }
-
-    auto jvmDllPath = java_home + L"/bin/server/jvm.dll";
-    HINSTANCE JVM_DLL = LoadLibraryW(jvmDllPath.c_str());
-    if (!JVM_DLL) {
-        std::cerr << "Can't load lib from: " << jvmDllPath.c_str() << "\n";
-        return;
-    }
-
-    createJavaVM = (CreateJavaVM_t) GetProcAddress(JVM_DLL, "JNI_CreateJavaVM");
-    findBootClass = (FindClassFromBootLoader_t) GetProcAddress(JVM_DLL, "JVM_FindClassFromBootLoader");
-    if (createJavaVM == nullptr || findBootClass == nullptr) {
-        std::cout << "GetProcAddress failed: "
-                  << reinterpret_cast<void*>(createJavaVM) << ", " << reinterpret_cast<void*>(findBootClass)
-                  << "\n";
-    }
-#endif
-    std::string java_home(javaHome);
-    auto javaDllPath = java_home + "/lib/libjava.dylib";
-    bool file_exists = fs::exists(javaDllPath);
-    auto size = file_exists && fs::is_regular_file(javaDllPath) ? static_cast<int64_t>(fs::file_size(javaDllPath)) : 0;
-    if (!size) {
-      std::cerr << "File don't exist: " << javaDllPath.c_str() << "\n";
-      return;
-    }
-
-    auto jvmDllPath = java_home + "/lib/server/libjvm.dylib";
-    void* JVM_DLL = dlopen(jvmDllPath.c_str(), RTLD_LAZY);
-    if (!JVM_DLL) {
-      std::cerr << "Can't load lib from: " << jvmDllPath.c_str() << "\n";
-      return;
-    }
-    std::cout << "Loaded: " << jvmDllPath.c_str() << "\n";
-    createJavaVM = (CreateJavaVM_t) dlsym(JVM_DLL, "JNI_CreateJavaVM");
-    auto findBootClass = (FindClassFromBootLoader_t) dlsym(JVM_DLL, "JVM_FindClassFromBootLoader");
-    if (createJavaVM == nullptr || findBootClass == nullptr) {
-      std::cout << "GetProcAddress failed: "
-                << reinterpret_cast<void*>(createJavaVM) << ", " << reinterpret_cast<void*>(findBootClass)
-                << "\n";
-    }
-  }
-}
